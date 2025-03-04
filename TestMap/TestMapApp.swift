e87bd7952a30ed5eda0ce1f42dd31570d2c1e99b
Yu Nagasaki <juonjuon19890913@gmail.com>
2025-03-05 01:21:52 +0900

    Initial Commit

---
+++ TestMap/TestMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMapApp.swift
+//  TestMap
+//
+//  Created by user on 2025/03/05.
+//
+
+import SwiftUI
+
+@main
+struct TestMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
